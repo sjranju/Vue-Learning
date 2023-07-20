d0f2a9b25631eff788a3bb009c06fa85289606e8
sjranju <sjranju@gmail.com>
2023-07-20 17:28:58 +0100

    Learnt Events, v-for, attribute binding & dynamic classes

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const app = Vue.createApp({
     data() {
         return {
-            showDetails: false,
-            name: 'Ranjana',
-            email: 'abc@xyz.com',
-            age: 27
+            url: "https://github.com/sjranju/Vue-Learning/tree/master",
+            showDetails: true,
+            students: [
+                { name: 'Ranjana', email: 'abc@xyz.com', age: 27, fav: true },
+                { name: 'Ranjan', email: 'xyz@abc.com', age: 31, fav: true },
+                { name: 'Unknowm', email: 'abc1@xyz.com', age: 35, fav: false }
+
+            ]
+
         }
     },
 
     methods: {
         hideDetails() {
             this.showDetails = !this.showDetails
+        },
+        handleToggleStudent(student) {
+            student.fav = !student.fav
+            console.log(student)
+
         }
     }
 })
